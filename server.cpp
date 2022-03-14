b5f19527dd683474921011c89eb5c04855ceae21
MengdlMole <96404439+MengdlMole@users.noreply.github.com>
2022-03-14 16:21:14 +0800

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -50,177 +50,180 @@ int main() {
 	int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_size);
 
 	printf("客户端( %s )已连接 \n", inet_ntoa(client_addr.sin_addr));
-    string i(50, '=');
-    cout << i << endl;
+    	string i(50, '=');
+    	cout << i << endl;
 
 
 	// 4. 实现功能：与客户端进行文件传输
-    char buffer[BUF_SIZE] = {0};
-    while(1){
+    	char buffer[BUF_SIZE] = {0};
+    	while(1)
+	{
 
-        printf("等待客户端请求... \n \n");
+        	printf("等待客户端请求... \n \n");
             
-        memset(buffer, 0, sizeof(buffer));
-        // 接收客户端请求
-        if( recv(client_socket, buffer, 1, 0) <= 0 ){
-            cout << buffer << endl;
-            printf("接收请求失败 \n");
-            break;
-        }
-        char str_temp[1];
-        strcpy(str_temp, buffer);
+        	memset(buffer, 0, sizeof(buffer));
+        	// 接收客户端请求
+        	if( recv(client_socket, buffer, 1, 0) <= 0 ){
+            		cout << buffer << endl;
+            		printf("接收请求失败 \n");
+            		break;
+        	}
+        	char str_temp[1];
+        	strcpy(str_temp, buffer);
         
-        // 1> 客户端上传文件
-        if( strcmp(str_temp, "1") == 0 ){
+        	// 1> 客户端上传文件
+        	if( strcmp(str_temp, "1") == 0 ){
                       
-            printf("接收：客户端请求上传文件 \n");
+            		printf("接收：客户端请求上传文件 \n");
 
-            memset(buffer, 0, sizeof(buffer));
-            strcpy(buffer, "服务端已接收请求");
-            // cout << sizeof(buffer) << endl;
-            if( send(client_socket, buffer, strlen(buffer), 0) <= 0 ){
-                printf("发送反馈失败 \n");
-                break;
-            }
-            cout << "发送：" << buffer << endl << endl;
+            		memset(buffer, 0, sizeof(buffer));
+            		strcpy(buffer, "服务端已接收请求");
+            		// cout << sizeof(buffer) << endl;
+            		if( send(client_socket, buffer, strlen(buffer), 0) <= 0 ){
+                		printf("发送反馈失败 \n");
+                		break;
+            		}
+            		cout << "发送：" << buffer << endl << endl;
             
-            // 接收保存文件的路径
-            memset(buffer, 0, sizeof(buffer));
-            if( recv(client_socket, buffer, BUF_SIZE, 0) <= 0 ){
-                printf("接收文件保存路径失败 \n");
-                break;
-            }
-            cout <<"接收：文件保存路径 " << buffer << endl;
-            char save_path[100];
-            strcpy(save_path, buffer);
+            		// 接收保存文件的路径
+            		memset(buffer, 0, sizeof(buffer));
+            		if( recv(client_socket, buffer, BUF_SIZE, 0) <= 0 ){
+                		printf("接收文件保存路径失败 \n");
+                		break;
+            		}
+            		cout <<"接收：文件保存路径 " << buffer << endl;
+            		char save_path[100];
+            		strcpy(save_path, buffer);
 
-            // 检测是否能够创建文件
-            FILE *fl = fopen(save_path, "wb");
-            memset(buffer, 0, sizeof(buffer));
-            if( fl == NULL ){
-                strcpy(buffer, "服务端无法创建文件");
-                if( send(client_socket, buffer, strlen(buffer),0) <= 0 ){
-                    printf("发送反馈失败 \n");
-                    fclose(fl);
-                    break;
-                }
-                cout << "发送：" << buffer << endl << endl;
-            }
-            else{
-                memset(buffer, 0, sizeof(buffer));
-                strcpy(buffer, "请发送文件");
-                if( send(client_socket, buffer, strlen(buffer), 0) <= 0 ){
-                    printf("发送反馈失败 \n");
-                    fclose(fl);
-                    break;
-                }
-                cout << "发送：" << buffer << endl << endl;
+            		// 检测是否能够创建文件
+            		FILE *fl = fopen(save_path, "wb");
+            		memset(buffer, 0, sizeof(buffer));
+            		if( fl == NULL ){
+                		strcpy(buffer, "服务端无法创建文件");
+                		if( send(client_socket, buffer, strlen(buffer),0) <= 0 ){
+                    			printf("发送反馈失败 \n");
+                    			fclose(fl);
+                    			break;
+                		}
+				fclose(fl);
+                		cout << "发送：" << buffer << endl << endl;
+            		}
+            		else{
+                		memset(buffer, 0, sizeof(buffer));
+                		strcpy(buffer, "请发送文件");
+                		if( send(client_socket, buffer, strlen(buffer), 0) <= 0 ){
+                    			printf("发送反馈失败 \n");
+                    			fclose(fl);
+                    			break;
+                		}
+                		cout << "发送：" << buffer << endl << endl;
 
-                // 接收文件
-                memset(buffer, 0, sizeof(buffer));
-                int nCount;
-                while( (nCount = recv(client_socket, buffer, BUF_SIZE, 0)) > 0 ){
-                    fwrite(buffer, nCount, 1, fl);
-                }
-                cout <<"接收：文件已接收" << endl;
+                		// 接收文件
+                		memset(buffer, 0, sizeof(buffer));
+                		int nCount;
+                		while( (nCount = recv(client_socket, buffer, BUF_SIZE, 0)) > 0 ){
+                    			fwrite(buffer, nCount, 1, fl);
+                		}
+                		cout <<"接收：文件已接收" << endl;
 
-                memset(buffer, 0, sizeof(buffer));
-                strcpy(buffer, "服务端已接收文件");
-                if( send(client_socket, buffer, strlen(buffer), 0) <= 0 ){
-                    printf("发送反馈失败 \n");
-                    fclose(fl);
-                    break;
-                }
-                cout << "发送：" << buffer << endl << endl;
-                printf("客户端( %s )已上传文件 \n", inet_ntoa(client_addr.sin_addr));
-                fclose(fl);
-                break;
-            }         
-        }
+                		memset(buffer, 0, sizeof(buffer));
+                		strcpy(buffer, "服务端已接收文件");
+                		if( send(client_socket, buffer, strlen(buffer), 0) <= 0 ){
+                    			printf("发送反馈失败 \n");
+                    			fclose(fl);
+                    			break;
+                		}
+                		cout << "发送：" << buffer << endl << endl;
+                		printf("客户端( %s )已上传文件 \n", inet_ntoa(client_addr.sin_addr));
+                		fclose(fl);
+                		break;
+            		}         
+        	}
         
-        // 2> 客户端下载文件
-        else {
+        	// 2> 客户端下载文件
+        	else {
                       
-            printf("接收：客户端请求下载文件 \n");
+            		printf("接收：客户端请求下载文件 \n");
 
-            memset(buffer, 0, sizeof(buffer));
-            strcpy(buffer, "服务端已接收请求");
-            // cout << sizeof(buffer) << endl;
-            if( send(client_socket, buffer, strlen(buffer), 0) <= 0 ){
-                printf("发送反馈失败 \n");
-                break;
-            }
-            cout << "发送：" << buffer << endl << endl;
+            		memset(buffer, 0, sizeof(buffer));
+            		strcpy(buffer, "服务端已接收请求");
+            		// cout << sizeof(buffer) << endl;
+            		if( send(client_socket, buffer, strlen(buffer), 0) <= 0 ){
+                		printf("发送反馈失败 \n");
+                		break;
+            		}
+            		cout << "发送：" << buffer << endl << endl;
             
-            // 接收目标文件的路径
-            memset(buffer, 0, sizeof(buffer));
-            if( recv(client_socket, buffer, BUF_SIZE, 0) <= 0 ){
-                printf("接收目标文件路径失败 \n");
-                break;
-            }
-            cout <<"接收：目标文件路径 " << buffer << endl;
-            char file_name[100];
-            strcpy(file_name, buffer);
+            		// 接收目标文件的路径
+            		memset(buffer, 0, sizeof(buffer));
+            		if( recv(client_socket, buffer, BUF_SIZE, 0) <= 0 ){
+                		printf("接收目标文件路径失败 \n");
+                		break;
+            		}
+            		cout <<"接收：目标文件路径 " << buffer << endl;
+            		char file_name[100];
+            		strcpy(file_name, buffer);
 
-            // 检测是否有目标文件
-            FILE *fl = fopen(file_name, "rb");
-            memset(buffer, 0, sizeof(buffer));
-            if( fl == NULL ){
-                strcpy(buffer, "服务端无此文件");
-                if( send(client_socket, buffer, strlen(buffer),0) <= 0 ){
-                    printf("发送反馈失败 \n");
-                    fclose(fl);
-                    break;
-                }
-                cout << "发送：" << buffer << endl << endl;
-            }
-            else{
-                memset(buffer, 0, sizeof(buffer));
-                strcpy(buffer, "请客户端接收文件...");
-                if( send(client_socket, buffer, strlen(buffer), 0) <= 0 ){
-                    printf("发送反馈失败 \n");
-                    fclose(fl);
-                    break;
-                }
-                cout << "发送：" << buffer << endl << endl;
-                
-                memset(buffer, 0, sizeof(buffer));
-                if( recv(client_socket, buffer, BUF_SIZE, 0) <= 0 ){
-                    printf("客户端无回应 \n");
-                    fclose(fl);
-                    break;
-                }
-                cout << "接收：" << buffer << endl;
+            		// 检测是否有目标文件
+            		FILE *fl = fopen(file_name, "rb");
+            		memset(buffer, 0, sizeof(buffer));
+            		if( fl == NULL ){
+                		strcpy(buffer, "服务端无此文件");
+                		if( send(client_socket, buffer, strlen(buffer),0) <= 0 ){
+                    			printf("发送反馈失败 \n");
+                    			fclose(fl);
+                    			break;
+                		}
+				fclose(fl);
+                		cout << "发送：" << buffer << endl << endl;
+            		}
+            		else{
+                		memset(buffer, 0, sizeof(buffer));
+                		strcpy(buffer, "请客户端接收文件...");
+                		if( send(client_socket, buffer, strlen(buffer), 0) <= 0 ){
+                    			printf("发送反馈失败 \n");
+                    			fclose(fl);
+                    			break;
+                		}
+                		cout << "发送：" << buffer << endl << endl;
+                		
+                		memset(buffer, 0, sizeof(buffer));
+                		if( recv(client_socket, buffer, BUF_SIZE, 0) <= 0 ){
+                    			printf("客户端无回应 \n");
+                    			fclose(fl);
+                    			break;
+                		}
+                		cout << "接收：" << buffer << endl;
 
-                // 发送文件
-                memset(buffer, 0, sizeof(buffer));
-                int nCount;
-                int i_temp = 1;
-                while ( (nCount = fread(buffer, 1, BUF_SIZE, fl)) > 0 ){
-                    if( send(client_socket, buffer, nCount, 0) <= 0 ){
-                        printf("文件发送失败 \n");
-                        i_temp = 0;
-                        fclose(fl);
-                        break;
-                    }
-                }
-                if ( i_temp == 0 ){ break; }
-                shutdown(client_socket, SHUT_WR); // 文件读取完毕，断开输出流，向服务端发送 FIN 包
-                cout <<"发送：文件已发送" << endl << endl;
+                		// 发送文件
+                		memset(buffer, 0, sizeof(buffer));
+                		int nCount;
+                		int i_temp = 1;
+                		while ( (nCount = fread(buffer, 1, BUF_SIZE, fl)) > 0 ){
+                	    		if( send(client_socket, buffer, nCount, 0) <= 0 ){
+                        			printf("文件发送失败 \n");
+                        			i_temp = 0;
+                        			fclose(fl);
+                        			break;
+                    			}
+                		}
+                		if ( i_temp == 0 ){ break; }
+                		shutdown(client_socket, SHUT_WR); // 文件读取完毕，断开输出流，向服务端发送 FIN 包
+                		cout <<"发送：文件已发送" << endl << endl;
 
-                memset(buffer, 0, sizeof(buffer));
-                if( recv(client_socket, buffer, BUF_SIZE, 0) <= 0 ){
-                    printf("客户端无回应 \n");
-                    fclose(fl);
-                    break;
-                }
-                cout << "接收：" << buffer << endl << endl;
-                printf("客户端( %s )已下载文件 \n", inet_ntoa(client_addr.sin_addr));
-                fclose(fl);
-                break;
-            }          
-        }
-    }
+                		memset(buffer, 0, sizeof(buffer));
+                		if( recv(client_socket, buffer, BUF_SIZE, 0) <= 0 ){
+                    			printf("客户端无回应 \n");
+                    			fclose(fl);
+                    			break;
+                		}
+                		cout << "接收：" << buffer << endl << endl;
+                		printf("客户端( %s )已下载文件 \n", inet_ntoa(client_addr.sin_addr));
+                		fclose(fl);
+                		break;
+            		}          
+        	}
+    	}
 
 
 
@@ -228,8 +231,8 @@ int main() {
 	close(server_socket);
 	close(client_socket);
 
-    cout << i << endl;
-    printf("通信结束 \n");
+    	cout << i << endl;
+    	printf("通信结束 \n");
 	return 0;
 
 }
